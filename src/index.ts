ccb455fecb83bb125cd053c8806e97e1b85dafef
Sano <68254575+sano0007@users.noreply.github.com>
2023-05-11 01:54:42 +0530

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import http from "http";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080/");
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
