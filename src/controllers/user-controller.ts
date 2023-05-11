5f004c470a85737a6876b7e70aabd5b3120c1646
Sano <68254575+sano0007@users.noreply.github.com>
2023-05-11 06:34:56 +0530

    feat: Create user-controller

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { UserUtils } from "../utils/user-utils";
+
+export const getAllUsers = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const users = await UserUtils.getUser();
+    return res.sendStatus(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
