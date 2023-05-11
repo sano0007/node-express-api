9b35985554d3a9b6b9112669e806ce892ccc7c8b
Sano <68254575+sano0007@users.noreply.github.com>
2023-05-11 07:53:48 +0530

    feat: Add isOwner middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { UserUtils } from "../utils/user-utils";
-import { merge } from "lodash";
+import { get, merge } from "lodash";
 
 export const isAuthenticated = async (
   req: express.Request,
@@ -21,6 +21,29 @@ export const isAuthenticated = async (
     }
     merge(req, { identity: currentUser });
     return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+    next();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
