909b12ada4a8afeb48eced380b030e82c11a5bd9
Sano <68254575+sano0007@users.noreply.github.com>
2023-05-11 06:29:58 +0530

    feat: Create index.ts middleware

---
+++ src/middlewares/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { UserUtils } from "../utils/user-utils";
+import { merge } from "lodash";
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies["SANO-AUTH"];
+
+    if (!sessionToken) {
+      return res.sendStatus(403);
+    }
+
+    const currentUser = await UserUtils.getUserBySessionToken(sessionToken);
+
+    if (!currentUser) {
+      return res.sendStatus(403);
+    }
+
+    merge(req, { identity: currentUser });
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
