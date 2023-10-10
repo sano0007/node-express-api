40eeb0929bdc2b51b74b42503d7e9d1ceb30a994
anarchy sano <pasindu.metaroon@gmail.com>
2023-10-10 14:44:45 +0530

    fix: route paths

--- src/router/user-router.ts
+++ src/router/user-router.ts
@@ -14,11 +14,11 @@ export default (router: express.Router) => {
   router.get("/users", isAuthenticated, getAllUsers);
   router.delete("/users/:id", isAuthenticated, isOwner, deleteUser);
   router.post("/users/update/:id", isAuthenticated, isOwner, updateUser);
-  router.post("/users/update/:id", isAuthenticated, isOwner, searchUsers);
-  router.get("/users/update/:id", isAuthenticated, isOwner, exportUserData);
-  router.post("/users/update/:id", isAuthenticated, isOwner, updateBio);
+  router.post("/users/search", isAuthenticated, isOwner, searchUsers);
+  router.get("/users/exportUser/:id", isAuthenticated, isOwner, exportUserData);
+  router.post("/users/updateBio/:id", isAuthenticated, isOwner, updateBio);
   router.post(
-    "/users/update/:id",
+    "/users/updateProfilePicture/:id",
     isAuthenticated,
     isOwner,
     updateProfilePicture
